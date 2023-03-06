cc65fddc5ffe5e684eaef7dde3982ab829a75ce2
Santhosh S <santhoshsekar2903@gmail.com>
2023-03-06 15:45:29 -0800

    CORS Check

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,33 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
-orgins = {
+origins = [
+    'http://botube-web.netlify.app/'
     'https://botube-web.netlify.app/',
-    'http://localhost:3000/'
-}
+    "http://localhost",
+    "http://localhost:300",
+]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["GET"],
+#     allow_headers=['Content-Type', 'application/xml']
+# )
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=orgins,
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET"],
-    allow_headers=['Content-Type', 'application/xml']
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
 async def root():
-    return ("06 March 2023")
+    return ("07 March 2023")
